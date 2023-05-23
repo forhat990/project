ae038f631fd441d17f348bf71677b225226ac79e
forhat <forhat990@gmail.com>
2023-05-23 23:48:28 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function() {
     jQuery('.arrival-area-carousel').owlCarousel({
         loop: true,
         autoplay: true,
-        margin: 75,
+        margin: 92.5,
         nav: false,
         dots: true,
         smartSpeed: 500,
